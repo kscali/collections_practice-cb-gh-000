f444ab6f08aaf06945eda6807c091c89627aa870
Koy Saeteurn <kosatun@aol.com>
2018-07-26 21:46:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(array)
+  sorted_array = array.sort
+end  
 
+def sort_array_desc(array)
+  sorted_desc_array = array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end  
+end  
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+end  
+  
+  
+  
